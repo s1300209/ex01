a86f3a7ff1b12d830c24d17bc1c4847947485e61
s1300209 <136040418+s1300209@users.noreply.github.com>
2023-06-14 12:33:43 +0900

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -1,1 +1,26 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+  int s1,s2,total;
+  char name[50];
+  srand(time(NULL));
+
+  printf("What is your name?\n");
+  printf(">");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  printf("Rolling the dice...\n");
+  s1 = rand() % 6 + 1;
+  s2 = rand() % 6 + 1;
+  printf("Die 1: %d\n",s1);
+  printf("Die 2: %d\n",s2);
+  total = s1 + s2;
+  printf("Total value: %d\n",total);
+  if(total > 7){
+    printf("%s won!\n",name);
+ }
+  else printf("%s lost!\n",name);
+
+  return 0;
+}
